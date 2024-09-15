9c5f7d90c231bb65770a5596c691d439c6af63b5
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-15 23:13:16 +0530

    chages made to conversation file

---
+++ 02.conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh : Hi Suresh, How are you?"
+echo "suresh : Hello Ramesh I am doing good hope you are doing godd"
+echo "Ramesh : "suresh I doing good, whats going on"
+echo "suresh : "I started learning shell scripting Ramesh"

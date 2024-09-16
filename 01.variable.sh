ab7fd89e1ccddf108d767c33307cb27607b0afb1
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-16 09:07:41 +0530

    added variables

---
+++ 01.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1 : Hi $person2, How are you?"
+echo "$person2 : Hello $person1 I am doing good hope you are doing godd"
+echo "$person1 : "$person2 I doing good, whats going on"
+echo "$person2 : "I started learning shell scripting $person1"

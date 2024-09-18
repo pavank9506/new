1eb0b3a9c7b61a13b5b47a44d8a3557694e5be37
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-18 22:27:53 +0530

    special characters

--- special variables.sh
+++ special variables.sh
@@ -1,3 +1,7 @@
 echo "all variable passed to the script $@"
 echo "all variable passed to the script $#"
 echo "script name $0"
+echo "current working directory $PWD"
+echo "home directory of the user who is running the script $HOME"
+echo "the pid of the script $$"
+echo "the PID of the background process $!"

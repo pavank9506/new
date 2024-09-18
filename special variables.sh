46a7e1ded228320d77139aa80e1431f6733ac190
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-18 22:36:07 +0530

    special characters

--- special variables.sh
+++ special variables.sh
@@ -4,4 +4,5 @@ echo "script name $0"
 echo "current working directory $PWD"
 echo "home directory of the user who is running the script $HOME"
 echo "the pid of the script $$"
+sleep 100 &
 echo "the PID of the background process $!"

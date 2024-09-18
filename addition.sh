d6d847c0613a914af913dc4fe219575a10cd5bd7
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-18 12:16:36 +0530

    Subtraction

--- addition.sh
+++ addition.sh
@@ -3,6 +3,6 @@ number2=$2
 
 subtraction=$(($number1-$number2))
 
-echo "the subtraction of $number1 and $number2 in $subtraction"
+echo "the subtraction of $number1 and $number2 is $subtraction"
 
 

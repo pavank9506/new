ededebf3bc19c2628e28b2903f5053c72ac37ded
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-21 20:00:40 +0530

    redone

--- installations.sh
+++ installations.sh
@@ -1,27 +1,53 @@
-#!/bin/bash
+# #!/bin/bash
+# userid=$(id -u)
+# if [ $userid -ne 0 ]
+# then
+#     echo "please run the script with admin previlages"
+#     exit 1
+# fi
+
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed installing"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "mysql is not installed please check"
+#         exit 1
+#     else
+#          echo "mysql is installed"
+#     fi
+# fi    
+
+
+# need to run the script using admin previlages
+# if not admin mode omit out telling, please run with admin credentials
+# check if the software already installed
+# if not please install the script
+# please install the script
+# mention if it is installed sucessfully or not.
+
 userid=$(id -u)
-if [ $userid -ne 0 ]
+if[ userid -ne 0 ]
 then
     echo "please run the script with admin previlages"
     exit 1
 fi
 
-dnf list installed mysql
+dnf list installed nginx 
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed installing"
-    dnf install mysql -y
+    echo "nginx not installed please installed"
+    dnf install nginx -y
     if [ $? -ne 0 ]
     then
-        echo "mysql is not installed please check"
+        echo "problem with the script please check"
         exit 1
     else
-         echo "mysql is installed"
+        echo "nginx installed"
     fi
-fi    
-
-
-
+fi            
 
 
 

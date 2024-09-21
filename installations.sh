d6c4b3c5b61ad251d9cef3d3688f3df3988b03c7
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-21 14:26:06 +0530

    changes made

--- installations.sh
+++ installations.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 userid=$(id -u)
 if [ $userid -ne 0 ]
+
 then
     echo "please run the script with admin previlages"
     exit 1
+
 fi
+
 dnf list installed git
+
 if [ $? -ne 0 ]
 then
-echo "git is not installed installing"
-dnf install git -y
+
+    echo "git is not installed installing"
+    dnf install git -y
+
 if [ $? -ne 0 ]
-echo "git is not installed please check"
-exit 1
+
+     echo "git is not installed please check"
+     exit 1
+
 fi
+
 else
 echo "git is installed"
+
 fi
 
 

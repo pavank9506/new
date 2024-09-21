37a693f2e132e23493c0f4ffe39871df2728e828
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-21 14:50:29 +0530

    my sql

--- installations.sh
+++ installations.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 userid=$(id -u)
 if [ $userid -ne 0 ]
-
 then
     echo "please run the script with admin previlages"
     exit 1
-
 fi
 
 dnf list installed mysql
-
 if [ $? -ne 0 ]
 then
-
     echo "mysql is not installed installing"
     dnf install mysql -y
-
-      if [ $? -ne 0 ]
-
-           echo "mysql is not installed please check"
-           exit 1
-           else
-               echo "mysql is installed"
-       fi
+    if [ $? -ne 0 ]
+    then
+        echo "mysql is not installed please check"
+        exit 1
+    else
+         echo "mysql is installed"
+    fi
+fi    
 
 
 

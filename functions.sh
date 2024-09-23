a62fbc6461aa66f36382bd66925e7bc448d6057e
Kota Ganesh <pavan.kota439@gmail.com>
2024-09-23 15:07:19 +0530

    git installation

--- functions.sh
+++ functions.sh
@@ -1,14 +1,49 @@
-#!/bin/bash
-userid=$(id -u)
+   #!/bin/bash
+   
+   userid=$(id -u)
 if [ $userid -ne 0 ]
 then
-    echo "please run the script wit admin previlages"
+    echo "please run the script with admin previlages"
     exit 1
 fi
 
-validate()
-          {
-            echo "exit status : $1"
-          }
-dnf list installed git
-validate $?          
+validate(){
+  if [ if $1 -ne 0 ]
+  then
+      echo "$2 is failed"
+      exit 1
+  else 
+      echo "$2 is sucess" 
+  fi       
+}
+dnf list installed nginx 
+validate $? "listing git"
+
+#     dnf install nginx -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "problem with the script please check"
+#         exit 1
+#     else
+#         echo "nginx installed"
+#     fi
+          
+
+
+# dnf list installed nginx 
+# if [ $? -ne 0 ]
+# then
+#     echo "nginx not installed please installed"
+#     dnf install nginx -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "problem with the script please check"
+#         exit 1
+#     else
+#         echo "nginx installed"
+#     fi
+# fi            
+
+
+
+#

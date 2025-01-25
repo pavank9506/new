14f2db453a47790d9a28122475921c13522795ca
Kota Ganesh <pavan.kota439@gmail.com>
2025-01-25 15:41:39 +0530

    variables

---
+++ newvariable.sh
@@ -0,0 +1,7 @@
+person1=jamjam
+person2=jamjum
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person1:: Hello $person2. I am fine. How are you doing?"
+echo "$person2:: I am doing good $person1. What's going on?"
+echo "$person1:: I started learning Shell Script $person2"
